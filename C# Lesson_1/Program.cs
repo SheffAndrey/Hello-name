9ff64a36e1f8e13da63db97f0e0cad9acf3b2c5a
Aydarov Andrey <aydarov.andrey@yandex.ru>
2023-04-02 23:13:20 +0300

    repositries

---
+++ C# Lesson_1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace C__Lesson_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Consoe.WriteLine("Введите имя - ");
+            
+        }
+    }
+}

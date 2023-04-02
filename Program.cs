0b0e8ee79251349c359e73238cfe46a2030c6ce5
Aydarov Andrey <aydarov.andrey@yandex.ru>
2023-04-03 00:28:45 +0300

    Test hello man

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace С__Aydarov_Andrey
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

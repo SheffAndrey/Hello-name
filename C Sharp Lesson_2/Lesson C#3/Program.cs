8123df98bc447ff87173cdc7c5595cab7200c3e4
Aydarov Andrey <aydarov.andrey@yandex.ru>
2023-04-04 01:27:52 +0300

    Сложение рандомных чисел

---
+++ C Sharp Lesson_2/Lesson C#3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Lesson_C_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число - ");
+            int numberA = Console.ReadLine();
+            Console.WriteLine("Введите второе число");
+            int numberB = Console.ReadLine();
+            int Sum = numberA + numberB;
+            Console.Write("Ответ на ваш запрос: ");
+            Console.WriteLine(Sum);
+        }
+    }
+}

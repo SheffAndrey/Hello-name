acfb31de0b748e6d29c08e7e9adc9d88cb39a3ce
Aydarov Andrey <aydarov.andrey@yandex.ru>
2023-04-05 23:40:30 +0300

    If-else

---
+++ Lesson if-else/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lesson_if_else
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Как зовут тебя человек?   -   ");
+            string username = Console.ReadLine();
+
+            if (username == "Андрей")
+            {
+                Console.WriteLine("Привет Андрюха");
+            }
+            else{
+                Console.WriteLine("Привет, ");
+                Console.WriteLine(username);
+            }
+        }
+    }
+}

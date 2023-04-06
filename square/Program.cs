00a2538c3e41fcf7e3a2d92343e70939279dba94
Aydarov Andrey <aydarov.andrey@yandex.ru>
2023-04-07 01:26:51 +0300

    Квадрат введеного пользователем числа

---
+++ square/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace square
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число: ");
+            int numb = Console.ReadLine();
+            int sqr = numb * numb;
+            Console.WriteLine(sqr);
+        }
+    }
+}
